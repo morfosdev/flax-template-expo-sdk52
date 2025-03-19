9f5a069c5b2f82e3b41f7ed81d20c798d5f0f4f3
Carlos A. <carlosvsk@gmail.com>
2025-03-19 14:42:29 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/IptTxtEdit.tsx
+++ src/tools/base/Elements/IptTxtEdit.tsx
@@ -23,70 +23,15 @@ export const IptTxtEdit = (props: Tprops) => {
   // ------- set IptTxt Inputs
   const { propsArray, stylesArray, funcsArray, path, args } = props.pass;
 
-  // --------------------------
-  // ----------- set COND VALUE
-  // --------------------------
-  const joinedPath = path.join('.'); // Corrigido para usar ponto como delimitador
+  console.log({ path });
+  const joinedPath = path.join('');
+  console.log({ joinedPath });
   const splitedPathArr = joinedPath.split('.');
+  console.log({ splitedPathArr });
+
   const idxToAdd = splitedPathArr.length - 2;
+  const editPath = splitedPathArr.splice(idxToAdd, 0, 'editData');
+  const newPath = splitedPathArr.splice(idxToAdd, 0, 'iptsChanges');
 
-  // Cria cópias separadas para evitar conflitos com splice
-  const splitedPathArrEdit = [...splitedPathArr];
-  splitedPathArrEdit.splice(idxToAdd, 0, 'editData');
-  const editPath = splitedPathArrEdit.join('.');
-
-  const splitedPathArrNew = [...splitedPathArr];
-  splitedPathArrNew.splice(idxToAdd, 0, 'iptsChanges');
-  const newPath = splitedPathArrNew.join('.');
-
-  // ------- set Data to Watch
-  const [sttText, setText] = React.useState('');
-
-  // Observar os dados nas paths separadas
-  const editData = useData(ct => pathSel(ct, splitedPathArrEdit));
-  const newData = useData(ct => pathSel(ct, splitedPathArrNew));
-
-  // Efeito colateral ao alterar editData
-  React.useEffect(() => {
-    if (!editData) {
-      // Se editData não existe, poderia processar algo com newPath
-      // Exemplo: console.log('editData vazio, usar newPath:', newPath);
-    }
-  }, [editData]);
-
-  // ---------- Functions
-  const getTxt = async (val: string) => {
-    for (const currFunc of funcsArray) {
-      await currFunc(val, args);
-    }
-  };
-
-  // ---------- Styles
-  const stlsUser = getStlValues(stylesArray);
-
-  // ------- set User Element Properties (If Exists)
-  const userElProps: any = {};
-  for (let strObj of propsArray) {
-    if (!strObj || typeof strObj !== 'string') continue;
-
-    const parsedObject = JSON5.parse(strObj);
-
-    for (const keyProp in parsedObject) {
-      const valueProp = parsedObject[keyProp];
-      const [hasVar, varValue] = getVarValue(valueProp);
-      userElProps[keyProp] = hasVar ? varValue : valueProp;
-    }
-  }
-
-  // Combina todas as props do TextInput
-  const allProps = {
-    style: stlsUser,
-    onChangeText: getTxt,
-    value: sttText,
-    placeholderTextColor: '#ccc',
-    placeholder: 'Escreva...',
-    ...userElProps,
-  };
-
-  return <TextInput {...allProps} />;
+  return <TextInput />;
 };
