d1e400bc3a4bb6895960780bd0ee6be283bc5fcd
Carlos A. <carlosvsk@gmail.com>
2025-03-19 14:43:20 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/IptTxtEdit.tsx
+++ src/tools/base/Elements/IptTxtEdit.tsx
@@ -23,15 +23,15 @@ export const IptTxtEdit = (props: Tprops) => {
   // ------- set IptTxt Inputs
   const { propsArray, stylesArray, funcsArray, path, args } = props.pass;
 
-  console.log({ path });
   const joinedPath = path.join('');
-  console.log({ joinedPath });
   const splitedPathArr = joinedPath.split('.');
-  console.log({ splitedPathArr });
 
   const idxToAdd = splitedPathArr.length - 2;
   const editPath = splitedPathArr.splice(idxToAdd, 0, 'editData');
   const newPath = splitedPathArr.splice(idxToAdd, 0, 'iptsChanges');
+
+  console.log({ editPath });
+  console.log({ newPath });
 
   return <TextInput />;
 };
