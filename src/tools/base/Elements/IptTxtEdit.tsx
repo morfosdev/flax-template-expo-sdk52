21dd830600c850b13cde8d64d10f74bbb1b2d6bc
Carlos A. <carlosvsk@gmail.com>
2025-03-19 14:17:49 -0300

    Commit generated by Flaxboll.

--- src/tools/base/Elements/IptTxtEdit.tsx
+++ src/tools/base/Elements/IptTxtEdit.tsx
@@ -1,10 +1,11 @@
 
 // ---------- import Packs
 import React from 'react';
+import JSON5 from 'json5';
 import { TextInput } from 'react-native';
 
 // ---------- import Local Tools
-import { getStlValues, pathSel } from '../project';
+import { getVarValue, getStlValues, pathSel } from '../project';
 import { useData } from '../../..';
 
 type Tprops = {
@@ -12,7 +13,7 @@ type Tprops = {
     propsArray: any;
     stylesArray: any;
     funcsArray: any;
-    editPath: string[];
+    path: string[];
     args: any;
   };
 };
@@ -20,45 +21,60 @@ type Tprops = {
 // IptTxtEdit - Entrada de Texto com prop value
 export const IptTxtEdit = (props: Tprops) => {
   // ------- set IptTxt Inputs
-  const { propsArray, stylesArray, funcsArray, editPath, args } = props.pass;
+  const { propsArray, stylesArray, funcsArray, path, args } = props.pass;
 
+  const fxFunction = () => {
+    if (editData) {
+    }
+    if (!editData) splitedPathArr.splice(idxToAdd, 0, 'iptsChanges');
+  };
+
+  // --------------------------
+  // ----------- set COND VALUE
+  // --------------------------
+  const joinedPath = editPath.join();
+  const splitedPathArr = joinedPath.split('.');
+  const idxToAdd = splitedPathArr.length - 2;
+  const editPath = splitedPathArr.splice(idxToAdd, 0, 'editData').join('.');
+  const newPath = splitedPathArr.splice(idxToAdd, 0, 'iptsChanges').join('.');
+
+  // ------- set Data to Watch
+  const [sttText, setText] = React.useState('');
+  const newData = useData(ct => pathSel(ct, splitedPathArr));
+  const editData = useData(ct => pathSel(ct, splitedPathArr));
+  React.useEffect(fxFunction, [editData]);
+
+  // ---------- Functions
   const getTxt = async (val: string) => {
     for (const currFunc of funcsArray) await currFunc(val, args);
   };
 
   // ---------- Styles
   const stlsUser = getStlValues(stylesArray);
-  const editData = useData(ct => pathSel(ct, editPath.join()));
 
-  // ------- set User Props
-  const userElProps = {};
+  // ------- set User Element Properties (If Exists)
+  const userElProps: any = {};
+  for (let strObj of propsArray) {
+    if (!strObj) continue;
+    if (!props) continue;
+    if (typeof strObj !== 'string') continue;
 
-  for (const object of propsArray) {
-    const isFnc = typeof object === 'function';
-    let newObj = {};
-    if (isFnc) {
-      console.log('IS A FUNCTION !!!!!!!!!!');
-      console.log({ object });
-      console.log({ newObj });
-      newObj = object();
-    }
+    const parsedObject = JSON5.parse(strObj);
 
-    if (!isFnc) {
-      console.log('NOT FUNCTION');
-      console.log({ object });
-      console.log({ newObj });
-      for (const keyProp in object) {
-        const valueProp = object[keyProp];
-        userElProps[keyProp] = valueProp;
-      }
+    for (const keyProp in parsedObject) {
+      const valueProp = parsedObject[keyProp];
+
+      const [hasVar, varValue] = getVarValue(valueProp);
+
+      if (hasVar) userElProps[keyProp] = varValue;
+      if (!hasVar) userElProps[keyProp] = valueProp;
     }
   }
 
-  console.log({ userElProps });
   const allProps = {
-    style: [stlsUser],
+    style: stlsUser,
     onChangeText: getTxt,
-    value: editData,
+    value: sttText,
     placeholderTextColor: '#ccc',
     placeholder: 'Escreva...',
 
@@ -67,4 +83,3 @@ export const IptTxtEdit = (props: Tprops) => {
 
   return <TextInput {...allProps} />;
 };
-
