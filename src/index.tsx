a6f05e0eb4c7dd5fb17ffaaa4081bdf9f412add6
Carlos A. <carlosvsk@gmail.com>
2025-03-20 13:45:13 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -163,7 +163,7 @@
  arrFunctions: [async (...args) =>
         functions.setVar({ args, pass:{
           keyPath: [`all.lists.lst2`],
-          value: [[{name: "Task1"}, {name: "Task2"}, {name: "Task3"}]]
+          value: ["$var_all.lists.lst3"]
         }})]
  , trigger: 'on press'
 }})],            childrenItems:[(...args:any) => <Elements.Text pass={{
