e2ae37227e15c659d34f4cbd0ccb15cc7f7cd2bd
Carlos A. <carlosvsk@gmail.com>
2025-04-10 11:39:19 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -96,7 +96,7 @@
 'lst2': [{name: "Tarefa 1", date: "21/03/25"},{name: "Tarefa 2", date: "21/03/25"},{name: "Tarefa 3", date: "24/03/25"}], 'lst3': [{name: "Task 1"},{name: "Task 2"}] } 
 , 
 'name': "Name:", 
-'dataToSet': {name: "Carlos", phone: "1111"}, 'newValue': red } 
+'dataToSet': {name: "Carlos", phone: "1111"}, 'newValue': "red" } 
  
 } 
  );
